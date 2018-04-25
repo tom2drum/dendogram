d14197b9f16664b489d95f51140ba72270ffafc5
tom-aglow <tom@ohhhh.me>
2018-04-25 10:55:40 +0300

    add data tree

---
+++ src/data/tree.js
@@ -0,0 +1,17 @@
+export default {
+  root: {
+    value: 'Artificial Intelligence',
+    left: {
+      value: 'Engine Intelligence',
+      left: {
+        value: 'Neuro-like Networks',
+      },
+      right: {
+        value: 'Heuristic Programming',
+      },
+    },
+    right: {
+      value: 'Engine Intelligence',
+    },
+  },
+};
